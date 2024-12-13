06d96dc24c706d88018cf21da3d972a422ab816a
Brian Drawert <briandrawert@gmail.com>
2024-12-12 16:44:37 -0800

    updated download

--- historical_gribs/download_historial_gribs.py
+++ historical_gribs/download_historial_gribs.py
@@ -4,12 +4,13 @@ import os.path
 
 
 year=2024
-days=range(1,15)
+days=range(1,22)
 hours=[0,6,12,18]
 
 #url = "https://noaa-gfs-bdp-pds.s3.amazonaws.com/gfs.20240701/00/atmos/gfs.t00z.pgrb2.0p25.f000"
 
-
+max_retries=3
+timeout=1
 
 for day in days:
     for hour in hours:
@@ -19,29 +20,37 @@ for day in days:
             url = url_base + url_path
             #
             output_file = f"{year}/{year}07{day:02}-{hour:02}-gfs.t{hour:02}z.pgrb2.0p25.f{fchr:03}"
-            #
             print(f"{url} -> {output_file}")
+            #
+            temp_output_file="in_progress_download"
+            retries = 0
             #
             if os.path.isfile(output_file):
                 print("  file exists")
             else:
-                try:
-                    temp_output_file="in_progress_download"
-                    # Make the request to download the GRIB2 file
-                    print(f"Starting download", end="", flush=True)
-                    response = requests.get(url, stream=True)
-                    response.raise_for_status()  # Check for HTTP errors
-                    
-                    # Save the file locally
-                    with open(temp_output_file, "wb") as file:
-                        for chunk in response.iter_content(chunk_size=819200):
-                            print(".",end='', flush=True)
-                            file.write(chunk)
-                    
-                    print(f"complete")
-                    os.rename(temp_output_file,output_file)
-                except requests.exceptions.RequestException as e:
-                    print(f"Error downloading GRIB2 file: {e}")
-                    raise e
-                
+                while retries < max_retries:
+                    try:
+                        # Make the request to download the GRIB2 file
+                        print(f"Starting download ({retries + 1})", end="", flush=True)
+                        response = requests.get(url, stream=True, timeout=timeout)
+                        response.raise_for_status()  # Check for HTTP errors
+                        
+                        # Save the file locally
+                        with open(temp_output_file, "wb") as file:
+                            for chunk in response.iter_content(chunk_size=819200):
+                                if chunk:
+                                    print(".",end='', flush=True)
+                                    file.write(chunk)
+                        
+                        print(f"complete")
+                        os.rename(temp_output_file,output_file)
+                        break
+                    except (requests.exceptions.Timeout, requests.exceptions.RequestException) as e:
+                        retries += 1
+                        print(f"\nError during download: {e}. Retrying ({retries}/{max_retries})...")
+                        time.sleep(1)  # Optional: Wait before retrying
+                if retries == max_retries:
+                    print("Max retries reached. Download failed.")
+                    raise Exception("Failed to download file after multiple attempts.")
+                            
 
